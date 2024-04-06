7423a2e8230af2e7e82ed00a50722d8ef1f002ad
Muxamil Mubashir <muzamil.mubashir@tkxel.io>
2024-04-07 01:28:39 +0500

    first

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,6 +3,6 @@ export const aboutData = {
   description1:
     "My name is Muzamil Mubashir. I'm a Mobile application developer located in Lahore, Pakistan.",
   description2:
-    "By day, I serve as a lead developer at a nearby agency, and during my evenings, I delve into freelance projects while dedicating time to crafting my own apps. I enjoy writing articles and poetry in my spare moments, and currently, I'm diving into the realms of machine learning and AI. I have a strong passion for learning and expanding my expertise in new fields.",
+    "By day, I serve as a lead developer at a nearby agency, and during my evenings, I delve into projects while dedicating time to crafting my own apps. I enjoy writing articles and poetry in my spare moments, and currently, I'm diving into the realms of machine learning and AI. I have a strong passion for learning and expanding my expertise in new fields.",
   image: 2,
 };
