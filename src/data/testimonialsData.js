495cde7df86bfe587498fc28dc808a11a3e45219
Muxamil Mubashir <muzamil.mubashir@tkxel.io>
2024-04-07 01:35:14 +0500

    first

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -11,14 +11,14 @@ export const testimonialsData = [
     id: 1,
     name: "Hassan Javed",
     title: "Business Partner | VP Engineering",
-    text: "Muzamil mubashir is an absolute tremendous talent with extreme hard work. His attitude towards analyzing and developing systems are amazing. i have always found him problem solving. His team leading skills are remarkable even with such a little experience in market. Very obedient and follows guidelines as it is. One of those i wish to work once again.",
+    text: "Muzamil mubashir is an absolute tremendous talent with extreme hard work. His attitude towards analyzing and developing systems are amazing. i have always found him problem solving.",
     image: boy2,
   },
   {
     id: 2,
     name: "Maryam Rafiq",
     title: "Sr. Software Qaulity Assurance Engineer",
-    text: "Muzamil has a tremendous potential for problem solving and writing a quality code. He is a good team player, an enthusiastic and vocal about his great software ideas.",
+    text: "Muzamil has a tremendous potential for problem solving and writing a quality code. He is an enthusiastic and vocal about his great software ideas.",
     image: girl2,
   },
 ];
